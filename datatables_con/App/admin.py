b496c31f486542f47623a912e21892b70268996f
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-14 13:06:20 +0300

    #Created super user to access admin page

--- datatables_con/App/admin.py
+++ datatables_con/App/admin.py
@@ -3,6 +3,7 @@ from App.models import Employee
 
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'occupation', 'gender']
+    search_fields = ['name']
     list_per_page = 8
 
 admin.site.register(Employee)
