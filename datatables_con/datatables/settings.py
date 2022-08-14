97d94379ef5ed480140e9925a5a1c334e1ee3d2e
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-14 13:09:48 +0300

    #Upload jazzmin to style admin page

--- datatables_con/datatables/settings.py
+++ datatables_con/datatables/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #Jazminn
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
